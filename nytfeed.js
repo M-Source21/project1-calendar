4e2d15704baf58f03bc09785ef1771c18556d9c7
M_Source <mabartolomeo@gmail.com>
2020-09-19 13:33:27 -0400

    completed javascript for NYT newsfeed

--- nytfeed.js
+++ nytfeed.js
@@ -12,6 +12,8 @@ $.ajax({
         createDiv(response.results[i].title);
         
         createDiv(response.results[i].abstract);
+
+        createDiv(response.results[i].url);
     }
 
     
@@ -22,7 +24,7 @@ function createDiv (contents){
     $("#article-section").append(`<div>${contents}</div>`);
 }
 
-//Adds two articles from 
+//Adds two articles from NYT science section 
 var queryURL = "https://api.nytimes.com/svc/topstories/v2/science.json?api-key=R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M"
 
 $.ajax({
@@ -34,6 +36,8 @@ $.ajax({
         createDiv(response.results[i].title);
         
         createDiv(response.results[i].abstract);
+
+        createDiv(response.results[i].url);
     }
 
     
@@ -44,3 +48,27 @@ function createDiv (contents){
     $("#article-section").append(`<div>${contents}</div>`);
 }
 
+
+//Adds two articles from NYT world news section 
+var queryURL = "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    for (i=0; i<2; i++){
+        
+        createDiv(response.results[i].title);
+        
+        createDiv(response.results[i].abstract);
+
+        createDiv(response.results[i].url);
+    }
+
+    
+});
+
+function createDiv (contents){
+
+    $("#article-section").append(`<div>${contents}</div>`);
+}
