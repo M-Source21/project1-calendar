cbd30784293dd8184ff6e9545bc61fa02192cc3d
Simon Lee <great.simonlee@gmail.com>
2020-09-20 16:30:03 -0400

    2020 Months added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Insert Weeks and Days in the Calendar
 function calendarStructure() {
   for (i = 0; i < 5; i++) {
     $("#calendarMonth").append(`<div id="weeks_${i + 1}"></div>`);
@@ -23,19 +24,40 @@ function calendarStructure() {
 
 calendarStructure();
 
+// Month
 var daysLeftGlobalMonth;
 var currentPageMonth;
-
-if ($("#monthName").text() == "September") {
-  var currentPageMonth = "09";
-} else if ($("#monthName").text() == "October") {
-  var currentPageMonth = "10";
-} else if ($("#monthName").text() == "November") {
-  var currentPageMonth = "11";
-} else if ($("#monthName").text() == "December") {
-  var currentPageMonth = "12";
+function currentMonth() {
+  if ($("#monthName").text() == "January") {
+    var currentPageMonth = "01";
+  } else if ($("#monthName").text() == "February") {
+    var currentPageMonth = "02";
+  } else if ($("#monthName").text() == "March") {
+    var currentPageMonth = "03";
+  } else if ($("#monthName").text() == "April") {
+    var currentPageMonth = "04";
+  } else if ($("#monthName").text() == "May") {
+    var currentPageMonth = "05";
+  } else if ($("#monthName").text() == "June") {
+    var currentPageMonth = "06";
+  } else if ($("#monthName").text() == "July") {
+    var currentPageMonth = "07";
+  } else if ($("#monthName").text() == "August") {
+    var currentPageMonth = "08";
+  } else if ($("#monthName").text() == "September") {
+    var currentPageMonth = "09";
+  } else if ($("#monthName").text() == "October") {
+    var currentPageMonth = "10";
+  } else if ($("#monthName").text() == "November") {
+    var currentPageMonth = "11";
+  } else if ($("#monthName").text() == "December") {
+    var currentPageMonth = "12";
+  }
 }
 
+currentMonth();
+
+// Insert days in the calendar
 function insertDaysIntoTable(month, num) {
   var daysLeftMonth = 7 - ((35 - (num + month.length)) % 7);
 
@@ -68,11 +90,34 @@ function insertDaysIntoTable(month, num) {
   daysLeftGlobalMonth = daysLeftMonth;
 }
 
-insertDaysIntoTable(September, 2);
+function insertDaysIntoTableMonthly(month) {
+  if (month === "January") {
+    insertDaysIntoTable(January, 3);
+  } else if (month === "February") {
+    insertDaysIntoTable(February, 5);
+  } else if (month === "March") {
+    insertDaysIntoTable(March, 0);
+  } else if (month === "April") {
+    insertDaysIntoTable(April, 3);
+  } else if (month === "May") {
+    insertDaysIntoTable(May, 5);
+  } else if (month === "June") {
+    insertDaysIntoTable(June, 1);
+  } else if (month === "July") {
+    insertDaysIntoTable(July, 5);
+  } else if (month === "August") {
+    insertDaysIntoTable(August, 6);
+  } else if (month === "September") {
+    insertDaysIntoTable(September, 2);
+  } else if (month === "October") {
+    insertDaysIntoTable(October, 4);
+  } else if (month === "November") {
+    insertDaysIntoTable(November, 0);
+  } else if (month === "December") {
+    insertDaysIntoTable(December, 2);
+  }
+}
 
-console.log(daysLeftGlobalMonth);
-
-// Previous and Next Month Buttons
 // Current month
 today = new Date();
 var currentMonth = $("#currentMonth")[0];
@@ -81,18 +126,27 @@ currentMonth.innerText = monthYear[month];
 // Previous Button
 $("#previousMonthBtn").on("click", function () {
   month--;
-  currentMonth.innerText = monthYear[month];
-  if (month == -1) {
-    month = 11;
-    currentMonth.innerText = monthYear[month];
+  let currentMonthIntoTable = monthYear[month];
+  currentMonth.innerText = currentMonthIntoTable;
+  if (month === 0) {
+    $("#previousMonthBtn").hide();
+  } else {
+    $("#nextMonthBtn").show();
   }
+  insertDaysIntoTableMonthly(currentMonthIntoTable);
 });
 // Next Button
 $("#nextMonthBtn").on("click", function () {
   month++;
-  currentMonth.innerText = monthYear[month];
-  if (month == 12) {
-    month = 0;
-    currentMonth.innerText = monthYear[month];
+  let currentMonthIntoTable = monthYear[month];
+  currentMonth.innerText = currentMonthIntoTable;
+  if (month === 11) {
+    $("#nextMonthBtn").hide();
+  } else {
+    $("#previousMonthBtn").show();
   }
+  insertDaysIntoTableMonthly(currentMonthIntoTable);
 });
+
+// Insert Initial Month / Day in Calendar
+insertDaysIntoTable(September, 2);
